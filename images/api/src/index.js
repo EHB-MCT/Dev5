414bbe376dcb6fc73ee0d15fdf494cbd0942d4fe
RobBosmans <56031137+RobBosmans@users.noreply.github.com>
2023-08-01 15:24:26 +0200

    Installed knex.js

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -2,10 +2,23 @@ const express = require("express");
 
 const app = express();
 
+//root route
 app.get("/", (request, response) => {
-    response.send({message:"test"})
+    //response.status(300).redirect("/info.html")
+    response.send("Hello world")
 })
 
+//GET: All users
+
+//GET: Users by ID
+
+//POST: save user
+
+//PUT: Update user
+
+//DELETE: delete user
+
+//port
 app.listen(3000, (err) => {
     if(!err){
         console.log("running on port" + 3000);
