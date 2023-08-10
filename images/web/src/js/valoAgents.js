33b6bfa1d13486b69191a6b28861e5d32dc05cda
RobBosmans <56031137+RobBosmans@users.noreply.github.com>
2023-08-10 17:18:04 +0200

    Agents now only show when clicked

--- images/web/src/js/valoAgents.js
+++ images/web/src/js/valoAgents.js
@@ -1,23 +1,47 @@
-async function getValorantAgents(){
-    const response = await fetch("https://valorant-api.com/v1/agents");
-    const data = await response.json();
-    const agents = data.data.filter(agent => agent.isPlayableCharacter);
+async function getValorantAgents() {
+  const response = await fetch("https://valorant-api.com/v1/agents");
+  const data = await response.json();
+  const agents = data.data.filter(agent => agent.isPlayableCharacter);
 
-    agents.forEach(agent => {
-      const myAgent = document.getElementById("agentContainer");
-      myAgent.insertAdjacentHTML("afterbegin", 
-        `<div id="agent">
-          <img src="${agent.fullPortrait}" alt="">
+  const agentIconsContainer = document.getElementById("agentIcons");
+  const agentContainer = document.getElementById("agentContainer");
+
+  agents.forEach(agent => {
+    agentIconsContainer.insertAdjacentHTML("afterbegin", 
+      `<div class="icon" id="${agent.displayName}-icon">
+        <img src="${agent.displayIcon}" alt="">
+      </div>`);
+
+    agentContainer.insertAdjacentHTML("afterbegin", 
+      `<div class="agent" id="${agent.displayName}-agent" style="display: none;">
+        <img id="agentImage" src="${agent.fullPortrait}" alt="">
+        <div id="agentInfo">
           <h3>${agent.displayName}</h3>
           <br>
           <h4>Role:</h4>
-          <p>${agent.role.displayName}</p>
+          <p> <img src="${agent.role.displayIcon}" alt=""> ${agent.role.displayName}</p>
           <br>
           <h4>Description:</h4>
           <p>${agent.description}</p>
-        </div>`);
+        </div>
+      </div>`);
+
+    const icon = document.getElementById(`${agent.displayName}-icon`);
+    const agentDetails = document.getElementById(`${agent.displayName}-agent`);
+
+    icon.addEventListener("click", () => {
+      // Hide all agent details
+      const allAgentDetails = document.querySelectorAll(".agent");
+      allAgentDetails.forEach(detail => {
+        detail.style.display = "none";
+      });
+
+      // Show the selected agent's details
+      agentDetails.style.display = "flex";
     });
+  });
 }
+
 
 // Function to fetch the API data and display weapons and skins
 /* async function fetchAndDisplayWeapons() {
