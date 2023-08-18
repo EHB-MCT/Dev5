67695d86e72e596882f18068837674038a7a5a20
RobBosmans <56031137+RobBosmans@users.noreply.github.com>
2023-08-18 17:15:14 +0200

    Added layout and styling to the threejs app

--- images/app/main.js
+++ images/app/main.js
@@ -1,42 +1,44 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 5000 );
-scene.add( camera );
+function threejs(){
+    //add scene and camera
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 5000 );
+    scene.add( camera );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    //init WebGL renderer
+    const renderer = new THREE.WebGLRenderer({ alpha: true});
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-const light = new THREE.AmbientLight( 0x404040, 100 ); // soft white light
-scene.add( light );
+    //add lighting
+    const light = new THREE.AmbientLight( 0x404040, 100 ); // soft white light
+    scene.add( light );
 
-const loader = new GLTFLoader();
-let agentModel;
+    const loader = new GLTFLoader();
+    let agentModel;
 
-loader.load( 'assets/deadlock/Deadlock.glb', function ( gltf ) {
-    agentModel = gltf;
-	scene.add( gltf.scene );
-    gltf.scene.rotation.y = 0;
+    //load gltf files
+    loader.load( 'assets/agents/Breach.glb', function ( gltf ) {
+        agentModel = gltf;
+        scene.add( gltf.scene );
+        gltf.scene.position.set(0, -0.2, -0.5);
+        gltf.scene.scale.set(0.2, 0.2, 0.2)
 
-    gltf.scene.position.y = -0.2;
-    gltf.scene.position.x = 0;
-    gltf.scene.position.z = -0.5;
+        animate();
+    },undefined, 
+    function ( error ) {
+        console.error( error );
+    } );
 
-    gltf.scene.scale.set(0.2, 0.2, 0.2)
-},
-function ( xhr ) {
-    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-},
-function ( error ) {
-	console.error( error );
-} );
-
-function animate() {
-	requestAnimationFrame( animate );
-    agentModel.scene.rotation.y += 0.02;
-	renderer.render( scene, camera );
+    //anim loop to display scene
+    function animate() {
+        requestAnimationFrame( animate );
+        agentModel.scene.rotation.y -= 0.01;
+        renderer.render( scene, camera );
+    }
 }
 
-animate();
+threejs();
+
